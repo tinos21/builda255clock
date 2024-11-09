382c2a331ef7d3a8177bb383be98b7db8b9d682f
tino <tkoumapl@email.essex.edu>
2024-11-09 02:04:43 -0500

    finished all html structure

--- src/clock.js
+++ src/clock.js
@@ -23,9 +23,9 @@ class Clock extends Component{
 
         return(
 
-
                <div>   
-             
+
+             <h1 class="title">    25 + 5 Clock        </h1>
                   {/*  break control level  */}
                 <div class="control-break-level"> 
                  <div id="break-label">Break Length</div>
@@ -38,11 +38,37 @@ class Clock extends Component{
                   </button>
                </div>
                {/*////////////////////////////////*/}
+               
+                    {/*  sension control level  */}
+              <div class="sension-break-level"> 
+                 <div id="session-label">Session Length</div>
+                   <button className="btn btn-primary" id="session-decrement" >
+                     <i className="bi bi-arrow-down" ></i>
+                    </button>
+               <div class="btn-level" id="session-length">25</div>
+                  <button className="btn btn-primary"  id="session-increment        ">
+                 <i className="bi bi-arrow-up"></i>     
+                  </button>
+               </div>
+                  {/*////////////////////////////////*/}
 
-    
+                   {/* session container here */}
+                   <div id="timer-label">         
+                     <h1> Session   </h1>
+
+                     <div id="time-left"> 35 </div>
+                   </div>    
+                   <div class="timer-control">
+                    <button id="start_stop">
+                     Start/Stop
+                    </button>
+                    <button id="reset">
+                      Reset
+                    </button>
+                        </div>
                 </div>
                
- 
+            
         );
 
      }
