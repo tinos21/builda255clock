a6fe1af3d898ce22dc4e7999afd14c6727d7bf84
tino <tkoumapl@email.essex.edu>
2024-11-09 01:24:06 -0500

    created break control level container

--- src/clock.js
+++ src/clock.js
@@ -11,6 +11,8 @@ class Clock extends Component{
 
          }
 
+
+
     }
 
 
@@ -21,12 +23,26 @@ class Clock extends Component{
 
         return(
 
-               <div>       </div>
 
+               <div>   
+             
+                  {/*  break control level  */}
+                <div class="control-break-level"> 
+                 <div id="break-label">Break Length</div>
+                   <button className="btn btn-primary" >
+                     <i className="bi bi-arrow-down"></i>
+                    </button>
+               <div class="btn-level" id="break-length">5</div>
+                  <button className="btn btn-primary"  id="break-increment">
+                 <i className="bi bi-arrow-up"></i>     
+                  </button>
+               </div>
+               {/*////////////////////////////////*/}
 
-
-
-
+    
+                </div>
+               
+ 
         );
 
      }
@@ -35,21 +51,4 @@ class Clock extends Component{
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+export default Clock;
