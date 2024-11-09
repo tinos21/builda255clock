426145523b6ccf9f726c06d4bf08f44388cd1f7b
tino <tkoumapl@email.essex.edu>
2024-11-09 00:04:13 -0500

    creating clock component

---
+++ src/clock.js
@@ -0,0 +1,55 @@
+
+import React, { Component } from 'react';
+
+
+class Clock extends Component{
+
+    constructor(props){
+     
+         super(props);
+         this.state = {
+
+         }
+
+    }
+
+
+
+
+
+    render() {
+
+        return(
+
+               <div>       </div>
+
+
+
+
+
+        );
+
+     }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
