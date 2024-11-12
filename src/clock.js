ba9330c274dd2b697288c70c333206e0447c528c
tino <tkoumapl@email.essex.edu>
2024-11-12 00:57:49 -0500

    creating element method

--- src/clock.js
+++ src/clock.js
@@ -8,14 +8,42 @@ class Clock extends Component{
      
          super(props);
          this.state = {
+            break_label:"break",
+            session_label:"Sessions",
+            breaklenght:5,
+            sessionlength:25,
 
          }
+
+    }
+  
+      /// handle upper arrow 
+    arrow_down_method= () =>{
+     
+
+    };
+
+    //// handle down arrow 
+    arrow_down_method= () =>{
+     
 
 
 
     }
 
 
+    formatTime = (time) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+        return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+      };
+      
+
+
+      start_stop_timer =()=>{
+        
+ 
+     }
 
 
 
@@ -26,46 +54,46 @@ class Clock extends Component{
                <div>   
 
              <h1 class="title">    25 + 5 Clock        </h1>
-                  {/*  break control level  */}
+                  {/*  break control level increase and decrease */}
                 <div class="control-break-level"> 
                  <div id="break-label">Break Length</div>
                    <button className="btn btn-primary" >
                      <i className="bi bi-arrow-down"></i>
                     </button>
-               <div class="btn-level" id="break-length">5</div>
+               <div class="btn-level" id="break-length">{this.state.breaklenght}</div>
                   <button className="btn btn-primary"  id="break-increment">
                  <i className="bi bi-arrow-up"></i>     
                   </button>
                </div>
                {/*////////////////////////////////*/}
                
-                    {/*  sension control level  */}
+                    {/*  sension control level increase and decrease  */}
               <div class="sension-break-level"> 
                  <div id="session-label">Session Length</div>
                    <button className="btn btn-primary" id="session-decrement" >
                      <i className="bi bi-arrow-down" ></i>
                     </button>
-               <div class="btn-level" id="session-length">25</div>
-                  <button className="btn btn-primary"  id="session-increment        ">
-                 <i className="bi bi-arrow-up"></i>     
+               <div class="btn-level" id="session-length">{this.state.sessionlength}</div>
+                  <button className="btn btn-primary"  id="session-increment ">
+                   <i className="bi bi-arrow-up"></i>     
                   </button>
                </div>
                   {/*////////////////////////////////*/}
 
                    {/* session container here */}
                    <div id="timer-label">         
-                     <h1> Session   </h1>
+                     <h1>{this.state.session_label}</h1>
 
-                     <div id="time-left"> 35 </div>
+                     <div id="time-left">{this.state.sessionlength}</div>
                    </div>    
-                   <div class="timer-control">
-                    <button id="start_stop">
+                  <div class="timer-control">
+                    <button id="start_stop" onClick={this.start_stop_timer}>
                      Start/Stop
                     </button>
                     <button id="reset">
                       Reset
                     </button>
-                        </div>
+                     </div>
                 </div>
                
             
